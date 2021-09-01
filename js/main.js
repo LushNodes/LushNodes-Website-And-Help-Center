1e2f90771ff4ab8dbf43b587563f77b0e136a199
AxelNetwork <87657295+H4rkEr@users.noreply.github.com>
2021-09-02 01:02:13 +0800

    Delete js directory

--- js/main.js
+++
@@ -1,8 +0,0 @@
-const collapsibles = document.querySelectorAll(".collapsible");
-collapsibles.forEach((item) =>
-  item.addEventListener("click", function () {
-    this.classList.toggle("collapsible--checked");
-  })
-);
-
-
